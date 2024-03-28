4f5e631a020ae6c9121329b21203a5f49f79b915
ibagi <bagiimi@gmail.com>
2024-03-28 22:58:39 +0100

    migration script

---
+++ scripts/migrate.ts
@@ -0,0 +1,12 @@
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { db } from '../api/db/db';
+import { migrate } from 'drizzle-orm/libsql/migrator';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+(async () => {
+  await migrate(db, {
+    migrationsFolder: resolve(__dirname, '../api/migrations'),
+  });
+})();
