c7d4ee5ee458325989166e0a34671510297c5a81
ibagi <bagiimi@gmail.com>
2024-03-31 10:32:28 +0200

    added trpc fetch adapter

--- api/trpc.ts
+++ api/trpc.ts
@@ -1,6 +1,15 @@
 import { initTRPC } from '@trpc/server';
+import { db } from './db';
 
 const t = initTRPC.create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
+
+export const apiProcedure = publicProcedure.use((opts) => {
+  return opts.next({
+    ctx: {
+      db,
+    },
+  });
+});
