0a78f025f0e24d6e373d7a22406730d6ed8696f4
ibagi <bagiimi@gmail.com>
2024-03-31 19:24:54 +0200

    request authorization

--- api/trpc.ts
+++ api/trpc.ts
@@ -1,15 +1,36 @@
-import { initTRPC } from '@trpc/server';
+import jwt from 'jsonwebtoken';
+import { TRPCError, initTRPC } from '@trpc/server';
+import { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
 import { db } from './db';
 
-const t = initTRPC.create();
+export async function createContext({ req }: FetchCreateContextFnOptions) {
+  if (!req.headers.has('authorization')) {
+    return { isAuthorized: false };
+  }
+
+  const headerParts = req.headers.get('authorization')?.split(' ');
+  if (headerParts?.length !== 2) {
+    return { isAuthorized: false };
+  }
+
+  try {
+    const token = headerParts[0];
+    const _ = jwt.verify(token, process.env.CLERK_PEM_PUBLIC_KEY!);
+    return { isAuthorized: true };
+  } catch {
+    return { isAuthorized: false };
+  }
+}
+
+const t = initTRPC.context<typeof createContext>().create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
-export const apiProcedure = publicProcedure.use((opts) => {
-  return opts.next({
-    ctx: {
-      db,
-    },
-  });
+export const apiProcedure = publicProcedure.use(({ ctx, next }) => {
+  if (!ctx.isAuthorized) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' });
+  }
+
+  return next({ ctx: { db } });
 });
