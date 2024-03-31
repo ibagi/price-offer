abbb53d12210bd9f91d84ca4c27911aff6fe3f5c
ibagi <bagiimi@gmail.com>
2024-03-31 19:24:54 +0200

    request authorization

--- functions/[path].ts
+++ functions/[path].ts
@@ -1,11 +1,12 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from '../api';
+import { createContext } from '../api/trpc';
 
 export const onRequest = async (opts) => {
   return fetchRequestHandler({
     endpoint: 'trpc',
     router: appRouter,
-    createContext: async ({ req }) => {},
+    createContext,
     req: opts.request,
   });
 };
