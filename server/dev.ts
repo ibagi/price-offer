c70117f761a6241d069ede6d7886b6bb0b1f50b6
ibagi <bagiimi@gmail.com>
2024-04-03 16:19:40 +0200

    add cors middleware for local dev server

--- server/dev.ts
+++ server/dev.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
+import cors from 'cors';
 import { createServer } from 'http';
 import { createHTTPHandler } from '@trpc/server/adapters/standalone';
 import { appRouter } from './router';
 import { db } from './db';
 import { initializeServices } from './services';
 
+
 const handler = createHTTPHandler({
+  middleware: cors(),
   router: appRouter,
   createContext: () => {
     return {
