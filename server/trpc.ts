793cc6d42660274da4585a2e3306e2bd48e32851
ibagi <bagiimi@gmail.com>
2024-04-07 09:01:47 +0200

    change error formating

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,14 +1,28 @@
 import superjson from 'superjson';
 import { TRPCError, initTRPC } from '@trpc/server';
 import { initializeServices } from './services';
+import { ZodError } from 'zod';
 
-export type Context = {
+interface Context {
   isAuthorized: boolean;
   services: ReturnType<typeof initializeServices>;
-};
+}
 
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
+  errorFormatter(opts) {
+    const { shape, error } = opts;
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === 'BAD_REQUEST' && error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
+  },
 });
 
 export const router = t.router;
